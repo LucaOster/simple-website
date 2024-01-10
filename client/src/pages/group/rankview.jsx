164f52c00ca92ea802dfb15f5b92b3d4ed602e5f
LucaOster <153316452+LucaOster@users.noreply.github.com>
2024-01-10 09:06:56 +0100

    Update rankview.jsx

--- client/src/pages/group/rankview.jsx
+++ client/src/pages/group/rankview.jsx
@@ -2,12 +2,6 @@ export default function RankView() {
 
     return (
         <>
-<<<<<<< HEAD
-            <div>
-                this is the Ranking View Page!!!
-            </div>
-        </>
-=======
         <header>
           <div class="navbar">
             <div class="dropdown">
@@ -66,7 +60,6 @@ export default function RankView() {
             </div>
     </div>*/}
       </>
->>>>>>> b36ccb55e92c37a43767a948f38a6e3732ad4c11
       )
 }
   
