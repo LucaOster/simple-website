ef17745340a7c205f89a4312be1b060b94936f1f
LucaOster <Developer0505@outlook.com>
2024-01-10 09:23:38 +0100

    update some files

--- server/routers/index.router.js
+++ server/routers/index.router.js
@@ -1,38 +1,4 @@
 require('dotenv').config()
-<<<<<<< HEAD
-// var express = require('express');
-// var jwt = require('express-jwt');
-// var path = require('path');
-// var authRouter = require('./auth.router');
-// var downloadController = require('../controllers/download.controller');
-// var errorHandler = require('../helpers/http-error-handler');
-
-var apiRouter = require('./api.router');
-
-const route = (app) => {
-  // app.use('/auth', authRouter);
-  // app.use(jwt({ 
-  //   secret: process.env.SECRET_KEY, 
-  //   algorithms: ['HS256'],
-  //   getToken: (req) => {
-  //     if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
-  //       return req.headers.authorization.split(' ')[1];
-  //     } else if (req.query && req.query.guid) {
-  //       return req.query.guid;
-  //     }
-  //     return null;
-  //   }
-  // }));
-  // app.use('/files/:path', downloadController.download);
- 
-  app.use('/api', apiRouter); // here error place
-
-  // global error handler
-  // app.use(errorHandler);
-} 
-
-module.exports = route;
-=======
 var express = require('express');
 var jwt = require('express-jwt');
 var path = require('path');
@@ -45,4 +11,3 @@ const route = (app) => {
   }
   
   module.exports = route;
->>>>>>> b36ccb55e92c37a43767a948f38a6e3732ad4c11
