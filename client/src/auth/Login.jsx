678d96e272e7e92a56ec77e68c10e121a522881d
LucaOster <153316452+LucaOster@users.noreply.github.com>
2024-01-10 08:55:02 +0100

    Update Login.jsx

--- client/src/auth/Login.jsx
+++ client/src/auth/Login.jsx
@@ -10,11 +10,7 @@ export default function Login(props) {
     if (localStorage.getItem('token') !== null && localStorage.getItem('token') !== undefined)
     {
       if (localStorage.getItem('token').split(" ")[0] === "Anaconda") {
-<<<<<<< HEAD
-          axios.post("http://localhost:5000/auth/", { token: localStorage.getItem("token") })
-=======
           axios.post("http://192.168.81.52:5000/auth/auth", { token: localStorage.getItem("token") })
->>>>>>> b36ccb55e92c37a43767a948f38a6e3732ad4c11
               .then((res) => {
                   if (res.data.message === "success_auth") {
                       window.location.href = "/";
@@ -32,11 +28,7 @@ export default function Login(props) {
 
   const handleClick = () => {
 
-<<<<<<< HEAD
-    axios.post("http://localhost:5000/login/", { user: { username: username, password: password} }).then((res) => {
-=======
     axios.post("http://192.168.81.52:5000/auth/login/", { user: { username: username, password: password} }).then((res) => {
->>>>>>> b36ccb55e92c37a43767a948f38a6e3732ad4c11
       if (res.data.message === "Login successfully") {
         
         swal({
