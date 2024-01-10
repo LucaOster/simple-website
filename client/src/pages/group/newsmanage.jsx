a93efd6f602cc15bf755445923254cd1631bf43d
LucaOster <153316452+LucaOster@users.noreply.github.com>
2024-01-10 09:05:23 +0100

    Update newsmanage.jsx

--- client/src/pages/group/newsmanage.jsx
+++ client/src/pages/group/newsmanage.jsx
@@ -5,44 +5,6 @@ import React, { useState, useEffect } from "react";
 
 export default function NewsManage() {
 
-    const [content, setContent] = useState('');
-
-    const handleClick = () => {
-
-        axios.post("http://localhost:5000/api/save_news/", { datas: { news_content: content, user_id: '1', type:'1'} }).then((res) => {
-          if (res.data.message === "successfully") {
-            
-            swal({
-              text: res.data.message,
-              icon: "success",
-              type: "success"
-            }).then(() => {
-              //window.location.href="/";
-            });
-            //localStorage.setItem("token", res.data.token);
-          }
-    
-      
-        });
-      }
-
-      const handleChange = (e) =>{
-
-        const content  = e.target.value
-        setContent(content)
-      }
-   
-    return (
-        <>
-            <div> 
-                This is the news manage page!!!
-                <textarea name='textcontent' onChange={handleChange}/>
-                <button type="button" className="btn" onClick={handleClick}>click me!</button>
-            </div>
-        </>
-=======
-export default function NewsManage() {
-
    
     return (
         <>
